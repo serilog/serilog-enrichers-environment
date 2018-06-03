03f76d2f5addcafab21ecd35b791db030541fa1b
Günther Foidl <gue@korporal.at>
2018-06-03 21:39:42 +0200

    Get MachineName in a "safe" way and included .netstandard1.5

--- src/Serilog.Enrichers.Environment/Enrichers/MachineNameEnricher.cs
+++ src/Serilog.Enrichers.Environment/Enrichers/MachineNameEnricher.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2013-2016 Serilog Contributors
+﻿// Copyright 2013-2018 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,12 +11,15 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
- 
 
 using System;
 using Serilog.Core;
 using Serilog.Events;
-using System.Runtime.InteropServices;
+using System.Runtime.CompilerServices;
+
+#if NETSTANDARD1_3
+using System.Net;
+#endif
 
 namespace Serilog.Enrichers
 {
@@ -39,16 +42,29 @@ namespace Serilog.Enrichers
         /// <param name="propertyFactory">Factory for creating new properties to add to the event.</param>
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
-#if ENV_USER_NAME
-            _cachedProperty = _cachedProperty ?? propertyFactory.CreateProperty(MachineNamePropertyName, Environment.MachineName);
-#else
-            var machineName = Environment.GetEnvironmentVariable("COMPUTERNAME");
-            if (string.IsNullOrWhiteSpace(machineName))
-                machineName = Environment.GetEnvironmentVariable("HOSTNAME");
+            logEvent.AddPropertyIfAbsent(GetLogEventProperty(propertyFactory));
+        }
 
-            _cachedProperty = _cachedProperty ?? propertyFactory.CreateProperty(MachineNamePropertyName, machineName);
+        private LogEventProperty GetLogEventProperty(ILogEventPropertyFactory propertyFactory)
+        {
+            // Don't care about thread-safety, in the worst case the field gets overwritten and one
+            // property will be GCed
+            if (_cachedProperty == null)
+                _cachedProperty = CreateProperty(propertyFactory);
+
+            return _cachedProperty;
+        }
+
+        // Qualify as uncommon-path
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static LogEventProperty CreateProperty(ILogEventPropertyFactory propertyFactory)
+        {
+#if NETSTANDARD1_3
+            var machineName = Dns.GetHostName();
+#else
+            var machineName = Environment.MachineName;
 #endif
-            logEvent.AddPropertyIfAbsent(_cachedProperty);
+            return propertyFactory.CreateProperty(MachineNamePropertyName, machineName);
         }
     }
-} 
+}
